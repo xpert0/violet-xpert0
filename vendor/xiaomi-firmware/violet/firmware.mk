91b2bc1de12370d779e2b2db89e149f268a37876
xpert0 <github.jaivs@slmail.me>
2024-08-31 14:52:39 +0530

    Initial Push

---
+++ vendor/xiaomi-firmware/violet/firmware.mk
@@ -0,0 +1,20 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/violet/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi-firmware/violet/aop.mbn:install/firmware-update/aop.mbn \
+    vendor/xiaomi-firmware/violet/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi-firmware/violet/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi-firmware/violet/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi-firmware/violet/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi-firmware/violet/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi-firmware/violet/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi-firmware/violet/imagefv.elf:install/firmware-update/imagefv.elf \
+    vendor/xiaomi-firmware/violet/km4.mbn:install/firmware-update/km4.mbn \
+    vendor/xiaomi-firmware/violet/splash.img:install/firmware-update/splash.img \
+    vendor/xiaomi-firmware/violet/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi-firmware/violet/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    vendor/xiaomi-firmware/violet/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi-firmware/violet/uefi_sec.mbn:install/firmware-update/uefi_sec.mbn \
+    vendor/xiaomi-firmware/violet/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi-firmware/violet/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi-firmware/violet/xbl_config.elf:install/firmware-update/xbl_config.elf
